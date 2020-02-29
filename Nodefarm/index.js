f5bf8168e2469fe2ac8a4d9a0118603343e8e7b9
PreetMangat <preetmangat93@outlook.com>
2020-02-28 22:05:09 -0600

    Finishing touches on initial project

--- Nodefarm/index.js
+++ Nodefarm/index.js
@@ -1,43 +1,14 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-//Sync
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-// const textOut = `This is what we know about: ${textIn}.\n Created on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt',textOut);
-// console.log('File written');
-
-//Async
-// fs.readFile('./txt/start.txt', 'utf-8', (error, data) => {
-//     console.log(data);
-// })
-
-// SERVER
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-    return output;
-}
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-
 
 const server = http.createServer((request,response) => {
     const {query,pathname} = url.parse(request.url, true);
